5742352853367e0e11c881e62982d995194f3959
caio.imperial <scaioimperial@gmail.com>
2025-10-14 23:16:21 -0300

    feat(backend): add module required and sqlite with DB

--- examples/backend/src/app.module.ts
+++ examples/backend/src/app.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module'
+import { CarModule } from './car/car.module';
+import { SeriesModule } from './series/series.module';
+import { CollectionItemModule } from './collection-item/collection-item.module';
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, CarModule, SeriesModule, CollectionItemModule],
   controllers: [AppController],
   providers: [AppService],
 })
